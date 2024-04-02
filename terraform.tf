04dbc28af1a3f5646e59fdee76666bdfa32d13b3
nbhagwat90 <nbhagwat90@gmail.com>
2024-04-02 07:01:21 +0000

    teraa file

---
+++ terraform.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket006"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
